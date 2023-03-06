3e48241e1d1c94b44ace4e032913983512e4028e
Mohammed Al Maghlouth <65549274+mohammedAlmaghlouth@users.noreply.github.com>
2023-03-06 21:31:44 +0300

    m

--- WEEK3/lecture3/hello/urls.py
+++ WEEK3/lecture3/hello/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #default url website/hello
-    path("", views.index, name ="index")
-    
+    # default url website/hello
+    path("", views.index, name="index"),
+    path("mohammed", views.mohammed, name="mohammed"),
+    path("<str:name>", views.greet, name="greet")
 ]
--- WEEK3/lecture3/hello/views.py
+++ WEEK3/lecture3/hello/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+
 def index(request):
     return HttpResponse("Hello world")
 
 
+def mohammed(request):
+    return HttpResponse("Hello, Mohammed")
+
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
